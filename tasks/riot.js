81042d12a94e6c108d5cbe6da3cc12f00d9c158e
LemondWU <lemond.wu@outlook.com>
2017-08-15 03:01:33 +0800

    Update riot.js

--- tasks/riot.js
+++ tasks/riot.js
@@ -11,124 +11,115 @@
 
 module.exports = function (grunt) {
 
-	var riot = require('riot'),
-		concat = require('concat-stream');
+    var riot = require('riot'),
+        concat = require('concat-stream');
 
-	var modularize = require('modularize');
+    var modularize = require('modularize');
 
-	var isType = function (type) {
-		return function (obj) {
-			return {}.toString.call(obj) == "[object " + type + "]";
-		};
-	};
+    var isType = function (type) {
+        return function (obj) {
+            return {}.toString.call(obj) == "[object " + type + "]";
+        };
+    };
 
-	grunt.registerMultiTask('riot', 'riot custom tag compiler plugin', function () {
-		var options = this.options({
-			compact : true ,
-			expr : true ,
-			type : null ,
-			template : null,
-			fileConfig : null,
-			concat : false,
-			modular: false
-		});
+    grunt.registerMultiTask('riot', 'riot custom tag compiler plugin', function () {
+        var options = this.options({
+            compact : true ,
+            expr : true ,
+            type : null ,
+            template : null,
+            fileConfig : null,
+            concat : false,
+            modular: false
+        });
 
-		var removeInvalidFiles = function(files) {
-			return files.src.filter(function(filepath) {
-				if (!grunt.file.exists(filepath)) {
-					grunt.log.warn('Source file "' + filepath + '" not found.');
-					return false;
-				} else {
-					return true;
-				}
-			});
-		};
+        var removeInvalidFiles = function(files) {
+            return files.src.filter(function(filepath) {
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+        };
 
-		var clone = function(obj) {
-			return JSON.parse(JSON.stringify(obj));
-		};
+        var clone = function(obj) {
+            return JSON.parse(JSON.stringify(obj));
+        };
 
-		var compileRiot = function(code, opts){
-			return riot.compile(code,opts);
-		};
+        var compileRiot = function(code, opts){
+            return riot.compile(code,opts);
+        };
 
-		var writeFile = function (path, output) {
-			grunt.file.write(path, output);
-			grunt.log.writeln('File ' + path + ' created.');
-		};
+        var writeFile = function (path, output) {
+            grunt.file.write(path, output);
+            grunt.log.writeln('File ' + path + ' created.');
+        };
 
-		function getOptions(file, options) {
-			return options.fileConfig ? options.fileConfig(file,clone(options)) : options;
-		}
+        function getOptions(file, options) {
+            return options.fileConfig ? options.fileConfig(file,clone(options)) : options;
+        }
 
-		function compileModular(source,config){
-			config = config || options;
+        function compileModular(source,config){
+            config = config || options;
 
-			if(config.modular){
+            if(config.modular){
 
-				if(!isType("Object")(config.modular)){
-					config.modular = {};
-				}
+                if(!isType("Object")(config.modular)){
+                    config.modular = {};
+                }
 
-				var modularConfig = config.modular ;
+                var modularConfig = config.modular ;
 
-				modularConfig.deps = modularConfig.deps || [];
+                modularConfig.deps = modularConfig.deps || [];
 
-				if(modularConfig.deps.indexOf('riot') < 0){
-					modularConfig.deps.unshift('riot');
-				}
+                if(modularConfig.deps.indexOf('riot') < 0){
+                    modularConfig.deps.unshift('riot');
+                }
 
-				if (modularConfig.exports) {
-					if (options.concat) {
-						grunt.log.warn('Cannot export "' + modularConfig.exports + '" if concat option is enabled.');
-					}
-					else {
-						source = 'var ' + modularConfig.exports + ' = ' + source;
-					}
-				}
+                modularConfig.input = source;
 
-				modularConfig.input = source;
+                return modularize(modularConfig);
+            }else{
+                return source;
+            }
+        }
 
-				return modularize(modularConfig);
-			}else{
-				return source;
-			}
-		}
+        var validFiles = this.files.map(function (files) {
+            return {
+                src: removeInvalidFiles(files),
+                dest: files.dest
+            };
+        });
 
-		var validFiles = this.files.map(function (files) {
-			return {
-				src: removeInvalidFiles(files),
-				dest: files.dest
-			};
-		});
+        validFiles.forEach(function (files, x) {
+            if(options.concat){
+                var strings = concat(function(out) {
+                    writeFile(
+                        files.dest ,
+                        compileModular(out)
+                    );
+                });
+                files.src.map(function(file, y){
+                    var fileSource = compileRiot( grunt.file.read(file) , getOptions(file,options) );
+                    strings.write(fileSource + '\n');
+                });
 
-		validFiles.forEach(function (files, x) {
-			if(options.concat){
-				var strings = concat(function(out) {
-					writeFile(
-						files.dest ,
-						compileModular(out)
-					);
-				});
-				files.src.map(function(file, y){
-					var fileSource = compileRiot( grunt.file.read(file) , getOptions(file,options) );
-					strings.write(fileSource + '\n');
-				});
+                strings.end();
+            }else{
+                files.src.map(function(file){
+                    writeFile(
+                        files.dest ,
+                        compileModular(
+                            compileRiot( grunt.file.read(file) , getOptions(file,options) ),
+                            getOptions(file,options)
+                        )
+                    );
+                });
+            }
+        });
 
-				strings.end();
-			}else{
-				files.src.map(function(file){
-					writeFile(
-						files.dest ,
-						compileModular(
-							compileRiot( grunt.file.read(file) , getOptions(file,options) ),
-							getOptions(file,options)
-						)
-					);
-				});
-			}
-		});
-
-	});
+    });
 
 };
